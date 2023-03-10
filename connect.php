2c2ffae96f69ffd555992307ea4e537089e63340
Albert Flores <106653612+rjflores747@users.noreply.github.com>
2023-03-10 14:50:41 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,9 +1,7 @@
 <?php
-
 @session_start();
 // change the url if you have a local 
-define('APP_URL','http://192.168.8.187/AutoHub-Group-Ticketing-System');
-
+define('APP_URL','https://ticketing-system.adrianpusana.com');
 require_once 'utility.php';
 // $array_data ['uri'] = 'https://autohub.ph/connect/api/v1/asa/api.php';
 // $array_data['parameters'] = http_build_query(array('key'=>'99799116300681219'));
@@ -12,13 +10,7 @@ require_once 'utility.php';
 
 
 // exit;
-    
 
-//   $servername = "localhost";
-//   $username = "root";
-//   // $username = "autoph_helpdesk";
-//   // $password = "AGc@2023#$@help@";
-//   $password = "";
   $servername = "ticketing-system.adrianpusana.com";
   $username = "syofdjax_alberto_flores";
   $password = "v,O@0OngL;}g";
@@ -32,5 +24,4 @@ require_once 'utility.php';
     die("Connection failed: " . $conn->connect_error);
   }
   // echo "Connected successfully";
-
 ?>
