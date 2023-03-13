6d16f2ea81bc1115a7afabf4bb3aa1ecaa8a0386
Alberto Flores <106653612+rjflores747@users.noreply.github.com>
2023-03-13 13:01:52 +0800

    updating all ticketing  table
    fix error in live

--- admin/ticket_table_container.php
+++ admin/ticket_table_container.php
@@ -33,7 +33,69 @@ if (!isset($_SESSION["id"])) {
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
   <br>
-  <?php include '../admin/ticket_support_table.php';?>
+  <?php 
+  // include '../admin/ticket_support_table.php';
+  ?>
+  
+    <div class="container">
+   
+          <h4>List of subject</h4>
+          <hr>
+          <table id="example1" class="table table-bordered">
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Ticket Number</th>
+                <th>Short Discription</th>
+                <th>Discription</th>
+                <th>Status</th>
+                <th>Action </th>
+	
+
+              </tr>
+            </thead>
+            <tbody>
+                <?php
+                require_once '../connect.php';
+              
+                $rolelistqry="SELECT * FROM ticket_incident";
+                $rolelistres=mysqli_query($conn,$rolelistqry);
+                while ($roledata=mysqli_fetch_assoc($rolelistres)) {
+                ?>
+                <tr>
+                  <td><?php echo $roledata['id'];?></td>
+                  <td><?php echo $roledata['ticket_number'];?></td>
+                  <td><?php echo $roledata['ticket_short_discrip'];?></td>
+                  <td><?php echo $roledata['ticket_discription'];?></td>
+                 
+                  <td>
+                    <?php  if ($roledata["ticket_status"] == 1) {
+                        echo '<span class="badge badge-primary">In Progress</span>';
+                      }else if ($roledata["ticket_status"] == 2){
+                        echo '<span class="badge badge-danger">Close</span>';
+                      }else if ($roledata["ticket_status"] == 3){
+                        echo '<span class="badge badge-success">New</span>' ;
+                      } else {
+                          echo "Inactive";
+                      }
+                    ?></td>
+                  <td>
+                    
+                    <a href="../admin/ticket_details_container.php?id=<?php echo $roledata['id'];?>" class="m-1 btn btn-sm btn-warning btn-icon"><i class="fas fa-eye"></i></a>
+                  
+                    
+                  </td>
+                  </tr>
+                  
+                <?php
+                }
+                ?>
+              
+            </tbody>
+          </table>
+      
+    </div>
+
   </div>
   <!-- /.content-wrapper -->
 
@@ -55,194 +117,28 @@ if (!isset($_SESSION["id"])) {
 </html>
 <!-- Page specific script -->
 <script>
-  var ticketlist = null;
-  var Toast = null;
-  // global variable
-  $(function() {
-      Toast = Swal.mixin({
-        toast: true,
-        position: 'top-end',
-        showConfirmButton: false,
-        timer: 3000
-      });
-      
-  
-    //for the deparment to deparment assignment
-    $(document).ready(function(){
-        $("#tkdepart").change(function(){
-          var tkdepart_id = this.value;
-          $.ajax({
-          url: "../admin/function_ticket_incident_ajax.php",
-          type: "POST",
-          data: {
-            tkdepart_id: tkdepart_id
-          },
-          cache: false,
-          success: function(result1){     
-            $("#deptgroup").html(result1);
-      
-          }
-          
-          });    
-        });  
-      });
-        $(document).ready(function(){
-            var empDataTable = $('#empTable').DataTable({
-              fnDrawCallback: function () {
-                    initActionRemove();
-                  },
-                'processing': true,
-                'serverSide': true,
-                'serverMethod': 'get',
-                
-                 ajax: {
-                    'url':'../admin/load_table_container.php',
-                  
-                    data: function (d) {
-                    return $.extend({}, d, {
-                        'type': 1,
-                    });
-                    },
-                },
-                pageLength: 5,
-                'columns': [
-                    { data: 'id' },
-                    { data: 'ticket_number' },
-                    { data: 'ticket_short_discrip' },
-                    { data: 'ticket_discription' },
-                    { data: 'ticket_status' ,
-                      render: function (data, type, row, meta) {
-                      //  alert(row['ticket_status']);
-                      if(row['ticket_status'] == 'Open'){
+ 
+  $(function () {
+    //Initialize Select2 Elements
+    $('.select2').select2()
 
-                        return '<span class="badge badge-info">Open</span>';
-                      }else if(row['ticket_status'] == '2' ){
-
-                        return '<span class="badge badge-danger">Close</span>' ;
-                      }else if(row['ticket_status'] == '3' ){
-
-                      return '<span class="badge badge-success">New</span>' ;
-                      }else if(row['ticket_status'] == 'Pending'){
-
-                        return '<span class="badge badge-warning">Pending</span> ';
-                      }else if(row['ticket_status'] == 'Success' ){
-                        return '<span class="badge badge-success">Success</span>';
-
-                      }else if(row['ticket_status'] == '1' ){
-
-                        return '<span class="badge badge-primary">In Progress</span>';
-                      }
-                    },},
-                      {
-                    title: "Action",
-                    data: null,
-                    orderable: true,
-                    width: "3%",
-                   
-                   
-
-                    render: function (data, type, row, meta) {
-
-                      var view = `<a href="../admin/ticket_details_container.php?id=`+
-                        row.id +
-                        `" style="cursor:pointer;" class="m-1 btn btn-sm btn-warning btn-icon" title="View"><i class="fas fa-eye"></i></a> `;
-                         // Insert visitor activity log into database 
-                      
-                    <?php   $ActivityLogs = mysqli_query($conn,"INSERT INTO `ticket_activity_logs`(`ticket_activity_uid`, `ticket_activity_name`, `ticket_activity_created_on`) VALUES ('".$_SESSION['u_id']."','View User ' ,NOW())");?>
-
-                      var update = `<a href="../admin/ticket_update_incident_container.php?id=`+
-                        row.id +
-                        `" style="cursor:pointer;" class="m-1 btn btn-sm btn-primary btn-icon" title="Edit"><i class="fas fa-pen"></i></a>`;
-                      var remove = `<a data-action-remove="`+row.id+`" style="cursor:pointer;" class="m-1 btn btn-sm btn-danger btn-icon" title="Remove"><i class="fa fa-trash"></i></a>`;
-                      
-                  
-                      if(row['ticket_user_role'] == '1' ){
-                        return (
-                        `
-                              <div class="row justify-content-center">
-                              `+view+update+remove+` 
-                              </div>
-                              `
-                      );
-                      }else if(row['ticket_user_role'] == '2'){
-                        return(`
-                        <div class="row justify-content-center">
-                              `+view+update+` 
-                              </div>
-                              
-                        `);
-                      }else if(row['ticket_user_role'] == '3'){
-                        return(`
-                        <div class="row justify-content-center">
-                              `+view+` 
-                              </div>
-                              
-                        `);
-                      }
-                    },
-                  },
-
-
-
-                ]
-            });
-            
-        });
-                function initActionRemove() {
-                  $("[data-action-remove]").each(function () {
-                    $(this).on("click", function () {
-                      var ticket_id = $(this).attr('data-action-remove');
-
-                        Swal.fire({
-                        title: "Are you sure, you want to remove this Ticket?",
-                        text: "This action cannot be undone.",
-                        icon: "warning",
-                        showCancelButton: true,
-                        confirmButtonColor: "#3085d6",
-                        cancelButtonColor: "#d33",
-                        confirmButtonText: "Yes, remove it!",
-                        showClass: {
-                          backdrop: "swal2-noanimation", // disable backdrop animation
-                          popup: "", // disable popup animation
-                          icon: "", // disable icon animation
-                        },
-                        hideClass: {
-                          popup: "", // disable popup fade-out animation
-                        },
-                      }).then((result) => {
-                        if (result.isConfirmed) {
-                          $.ajax({
-                            url: "function_delete_ticket_incident.php",
-                            data: {
-                              ticket_id: ticket_id, 
-                            },
-                            type: "POST",
-                            dataType: "json",
-                            beforeSend: function () {
-                              // toast("info", "Updating...");
-                            },
-                            success: function (result) {
-                              Toast.fire({
-                                icon: 'success',
-                                title: 'SucesssFul Deleted.'
-                              }); 
-                              
-                              $("#empTable").DataTable().ajax.reload();
-                            },
-                            error: function () {
-                              // toast("error", "Error has occurred. Try again.");
-                            },
-                          });
-                        }
-                      });
-
-
-                      return false;
-                    
-                    });
-                  });
-                  
-                } 
-      });
-        
+    //Initialize Select2 Elements
+    $('.select2bs4').select2({
+      theme: 'bootstrap4'
+    })
+    $("#example1").DataTable({
+      "responsive": true, "lengthChange": false, "autoWidth": false,
+      // "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+      "buttons": ["colvis"]
+    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    $('#example1').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
+      "responsive": true,
+    });
+  });
 </script>
