2ce515a01bd35d74fe8020dcb8283b82c627ab1b
Alberto Flores <106653612+rjflores747@users.noreply.github.com>
2023-03-17 10:50:42 +0800

    updating

--- admin/ticket_incident.php
+++ admin/ticket_incident.php
@@ -68,7 +68,7 @@ require_once '../connect.php';
           </div>
           <div class="col-7"> 
              <!-- <form action="" method="post"> -->
-                <form action="../admin/function_create_ticket_incident.php" method="POST">
+                <form action="../admin/ticket_details_container.php" method="POST">
                   <div class="form-group">
                     <label for="inputeparment">Department</label>
                     <select class="form-control select2bs4"  name="inputeparment" id="inputeparment" style="width: 100%;" require>
