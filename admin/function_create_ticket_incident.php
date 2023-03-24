8084d88567c435370c1af8a948a95f7a351b2bf4
Alberto Flores <106653612+rjflores747@users.noreply.github.com>
2023-03-24 11:14:15 +0800

    updating function

--- admin/function_create_ticket_incident.php
+++ admin/function_create_ticket_incident.php
@@ -6,48 +6,45 @@ require_once '../connect.php';
 // 		header("Location: index.php");
 // 		exit(); 
 // 	}
-// use LDAP\Result;
-// if (isset($_POST["inputSubject"]) && isset($_POST["inputMessage"])&&isset($_POST["inputeparment"])) {
-if (isset($_POST["submit"])) {
+use LDAP\Result;
+if (isset($_POST["var_department"]) && isset($_POST["var_subject"])&&isset($_POST["var_message"])) {
     $date = date('yyyy-m-d h:i:s');
-    $departmentType = $_POST['inputSubject'];
-    $sortdiscription = $_POST['inputMessage'];
-    $discription = $_POST['inputeparment'];
+    $departmentType = $_POST['var_department'];
+    $sortdiscription = $_POST['var_subject'];
+    $discription = $_POST['var_message'];
     $fn = $_SESSION['ticket_fn']. $_SESSION['ticket_ln']; 
     // $user_id = $_SESSION['user_id'];
     $employee_id = $_SESSION['id'];
     // $sortdiscription = $_POST['inputSubject'];
     // $discription = $_POST['inputMessage'];
     // $departmentType = $_POST['inputeparment'];
-    $return_arrr = array();
+    $return_arr = array();
    
         $sql = "INSERT INTO ticket_incident(
-        id ,
-        ticket_number ,
-        u_id ,
-        ticket_caller ,
-        ticket_category ,
-        ticket_subcategory ,
-        ticket_service ,
-        ticket_config_item ,
-        ticket_short_discrip ,
-        ticket_discription ,
-        ticket_filedownload ,
-        ticket_contact_type ,
-        ticket_status ,
-        ticket_imapact ,
-        ticket_urgent ,
-        ticket_priority ,
-        ticket_assign_group ,
-        ticket_assign_to ,
-        ticket_department_id ,
-        ticket_timeofdate ,
-        ticket_timeofdate_end ) 
+        id,
+        u_id,
+        ticket_number, 
+        ticket_caller, 
+        ticket_category, 
+        ticket_subcategory, 
+        ticket_service, 
+        ticket_config_item, 
+        ticket_short_discrip, 
+        ticket_discription, 
+        ticket_contact_type, 
+        ticket_status, 
+        ticket_imapact, 
+        ticket_urgent, 
+        ticket_priority, 
+        ticket_assign_group, 
+        ticket_department_id, 
+        ticket_timeofdate, 
+        ticket_timeofdate_end) 
         VALUES 
         ('',
-        '$employee_id',
+        '".$employee_id."',
         '',
-        '$fn',
+        '".$fn."',
         '',
         '',
         '',
@@ -55,13 +52,12 @@ if (isset($_POST["submit"])) {
         '$sortdiscription',
         '$discription',
         '',
-        '',
         '3',
         '',
         '',
         '',
         '',
-        '',
+        
         '$departmentType',
         NOW(),
         '');";
@@ -74,37 +70,26 @@ if (isset($_POST["submit"])) {
             $ticket_number = "ATK_".$code."_".$last_id;
             $query = "UPDATE ticket_incident SET ticket_number = '".$ticket_number."', u_id = '".$employee_id."'  WHERE id = '".$last_id."'";      
             $res = mysqli_query($conn,$query);
-            $return_arrr['id'] = $last_id; // success
+            $return_arr['id'] = $last_id; // success
             }
             
-        }   
-        if($result){
-            $_SESSION['status'] = 1;
-print_r($result);exit;
-
-            echo '<script> alert(" Insert Successful"); </script>';
-
-        }else{
-            $_SESSION['status'] = 0;
-            echo '<script> alert("Insert not Successful"); </script>';
-print_r($result);exit;
         }
-        // if($result > 0)
-        // {
-        //   $return_arr['status'] = 1;
+        if($result > 0)
+        {
+          $return_arr['status'] = 1;
          
-        // }
-        // else
-        // {
-        //     // echo '<script> alert("Data Not Deleted"); </script>';
-        //     $return_arr['status'] = 0;
-        // } 
+        }
+        else
+        {
+            // echo '<script> alert("Data Not Deleted"); </script>';
+            $return_arr['status'] = 0;
+        } 
         
          // Insert visitor activity log into database 
             $ActivityLogs = mysqli_query($conn,"INSERT INTO `ticket_activity_logs`(`ticket_activity_uid`, `ticket_activity_name`, `ticket_activity_created_on`) VALUES ('".$_SESSION['id']."','You have successfully add new ticket' ,NOW())");
       
-            echo json_encode($result);
-        header("location: ../admin/ticket_details_container.php?id=".$last_id);
+            echo json_encode($return_arr);
+        // header("location: ../admin/ticket_details_container.php?id=".$last_id);
        
 }
 ?>
