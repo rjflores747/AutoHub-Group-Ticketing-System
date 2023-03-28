9d9fa30716ee2e7fa6ce0199efbf71f25718b104
Alberto Flores <106653612+rjflores747@users.noreply.github.com>
2023-03-28 10:18:56 +0800

    updating function

--- admin/function_create_ticket_incident.php
+++ admin/function_create_ticket_incident.php
@@ -76,11 +76,14 @@ if (isset($_POST["var_department"]) && isset($_POST["var_subject"])&&isset($_POS
         }
         if($result > 0)
         {
+            print_r($result);
           $return_arr['status'] = 1;
          
         }
         else
         {
+            
+            print_r($result);
             // echo '<script> alert("Data Not Deleted"); </script>';
             $return_arr['status'] = 0;
         } 
