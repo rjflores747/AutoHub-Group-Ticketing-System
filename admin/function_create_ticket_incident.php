f1e832f3ff9d4ccfd34fcc6a110bd8224c7dd997
Alberto Flores <106653612+rjflores747@users.noreply.github.com>
2023-03-30 10:59:52 +0800

    updating

--- admin/function_create_ticket_incident.php
+++ admin/function_create_ticket_incident.php
@@ -1,5 +1,106 @@
 
 <?php
+
+require_once '../connect.php';
+// if (!isset($_SESSION["id"])) {
+// 		header("Location: index.php");
+// 		exit(); 
+// 	}
+use LDAP\Result;
+// if (isset($_POST["var_department"]) && isset($_POST["var_subject"])&&isset($_POST["var_message"])) {
+    if (isset($_POST["submit"])) {
+    $date = date('yyyy-m-d h:i:s');
+    $departmentType = $_POST['inputeparment'];
+    $sortdiscription = $_POST['inputSubject'];
+    // $discription = $_POST['inputMessage'];
+    $discription = addslashes($_POST['inputMessage']);  
+
+    $fn = $_SESSION['ticket_fn']. $_SESSION['ticket_ln']; 
+    // $user_id = $_SESSION['user_id'];
+    $employee_id = $_SESSION['id'];
+    // $sortdiscription = $_POST['inputSubject'];
+    // $discription = $_POST['inputMessage'];
+    // $departmentType = $_POST['inputeparment'];
+    $return_arr = array();
+   
+        $sql = "INSERT INTO ticket_incident(
+        id,
+        u_id,
+        ticket_number, 
+        ticket_caller, 
+        ticket_category, 
+        ticket_subcategory, 
+        ticket_service, 
+        ticket_config_item, 
+        ticket_short_discrip, 
+        ticket_discription, 
+        ticket_contact_type, 
+        ticket_status, 
+        ticket_imapact, 
+        ticket_urgent, 
+        ticket_priority, 
+        ticket_assign_group, 
+        ticket_department_id, 
+        ticket_timeofdate, 
+        ticket_timeofdate_end) 
+        VALUES 
+        ('',
+        '".$employee_id."',
+        '',
+        '".$fn."',
+        '',
+        '',
+        '',
+        '',
+        '$sortdiscription',
+        '$discription',
+        '',
+        '3',
+        '',
+        '',
+        '',
+        '',
+        
+        '$departmentType',
+        NOW(),
+        '');";
+       
+        $result = mysqli_query($conn,$sql);
+        // print_r($result); 
+         
+        if ($result) {
+            // print_r("testing");
+            $last_id = mysqli_insert_id($conn);
+            if($last_id){
+            $code = rand(1,99999);
+            $ticket_number = "ATK_".$code."_".$last_id;
+            $query = "UPDATE ticket_incident SET ticket_number = '".$ticket_number."', u_id = '".$employee_id."'  WHERE id = '".$last_id."'";      
+            $res = mysqli_query($conn,$query);
+            $return_arr['id'] = $last_id; // success
+            }
+            if($result > 0)
+            {
+              $return_arr['status'] = 1;
+             
+            }
+            else
+            {
+                // echo '<script> alert("Data Not Deleted"); </script>';
+                $return_arr['status'] = 0;
+            } 
+            
+        }
+      
+        
+         // Insert visitor activity log into database 
+         $ActivityLogs = mysqli_query($conn,"INSERT INTO `ticket_activity_logs`(`ticket_activity_uid`, `ticket_activity_name`, `ticket_activity_created_on`) VALUES ('".$_SESSION['id']."','You have successfully add new ticket' ,NOW())");
+      
+            echo json_encode($return_arr);
+        // header("location: ../admin/ticket_details_container.php?id=".$last_id);
+       
+}
+?>
+<?php
 require_once '../connect.php';
 
 // if (!isset($_SESSION["id"])) {
@@ -66,6 +167,7 @@ if(isset($_POST["submit"]))
     '')";
     $query_run = mysqli_query($conn, $queryMessage); // echo $query; exit;
     $insertedmessageid = mysqli_insert_id($conn);
+    if($insertedmessageid){
     $code = rand(1,99999);
     $ticket_number = "ATK_".$code."_".$insertedmessageid;
     $query = "UPDATE ticket_incident SET ticket_number = '".$ticket_number."', u_id = '".$employee_id."'  WHERE id = '".$insertedmessageid."'";      
@@ -82,8 +184,11 @@ if(isset($_POST["submit"]))
         
         $return_arr['status'] = 0;
     }
+    }
 
     echo json_encode($return_arr);
+        // header("location: ../admin/ticket_details_container.php?id=".$last_id);
+
 }
 
 ?>
