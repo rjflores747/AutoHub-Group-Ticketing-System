e8ec6e2062e034e3b8acd50adaeb2bc227771db5
Alberto Flores <106653612+rjflores747@users.noreply.github.com>
2023-04-01 11:17:13 +0800

    updating function
    testing

--- admin/function_create_ticket_incident.php
+++ admin/function_create_ticket_incident.php
@@ -1,92 +1,109 @@
 
 <?php
 require_once '../connect.php';
-
-if (!isset($_SESSION["id"])) {
-  header("Location: index.php");
-  exit();
- 
-}
-// if(isset($_POST['ticket_id']))
-// if(isset($_POST["submit"]))
-if (isset($_POST["inputeparment"]) && isset($_POST["inputSubject"])&&isset($_POST["inputMessage"])) 
-
+$query = "SELECT ticket_number FROM ticket_incident ORDER BY ticket_number DESC";
+$result = mysqli_query($conn,$query);
+$row = mysqli_fetch_array($result);
+$lastid = $row['id'];
+if(empty($lastid))
 {
-    $departmentType = $_POST['inputeparment'];
-    $sortdiscription = $_POST['inputSubject'];
-    // $user_id = $_SESSION['id'];
-    // $id = $_POST['ticket_id'];
-    $message = addslashes($_POST['inputMessage']);  
-    $fn = $_SESSION['ticket_fn']. $_SESSION['ticket_ln']; 
-    $employee_id = $_SESSION['id'];
-    $return_arr = array(); 
-    
-    // $queryMessage = "INSERT INTO `ticket_chat`(`ticket_id`, `ticket_user_id`, `ticket_date_time`, `ticket_message`, `ticket_status`) VALUES ('$id','$user_id',NOW(),'$message','1')"; 
-   $queryMessage ="INSERT INTO `ticket_incident`(
-    `id`, 
-    `ticket_number`, 
-    `u_id`, 
-    `ticket_caller`, 
-    `ticket_category`, 
-    `ticket_subcategory`, 
-    `ticket_service`, 
-    `ticket_config_item`, 
-    `ticket_short_discrip`, 
-    `ticket_discription`, 
-    `ticket_filedownload`, 
-    `ticket_contact_type`, 
-    `ticket_status`, 
-    `ticket_imapact`, 
-    `ticket_urgent`,
-    `ticket_priority`, 
-    `ticket_assign_group`,
-    `ticket_assign_to`, 
-    `ticket_department_id`, 
-    `ticket_timeofdate`,
-    `ticket_timeofdate_end`) 
-   VALUES (
-    '',
-    '',
-    '$employee_id',
-    '$fn',
-    '',
-    '',
-    '',
-    '',
-    '$sortdiscription',
-    '$message',
-    '',
-    '',
-    '3',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '$departmentType',
-    NOW(),
-    '')";
-    $query_run = mysqli_query($conn, $queryMessage); // echo $query; exit;
-    $insertedmessageid = mysqli_insert_id($conn);
-    $last_id = mysqli_insert_id($conn);
-    var_dump($insertedmessageid);
-    exit();
-      if($insertedmessageid)
-      {
-        $return_arr['status'] = 1;
-        
-      
-      }
-      else
-      {
-          
-          $return_arr['status'] = 0;
-      }
-
-
-    echo json_encode($return_arr);
-        header("location: ../admin/ticket_details_container.php?id=".$insertedmessageid);
-
+    $number = "ATK-0000001";
+}
+else
+{
+    $idd = str_replace("ATK-", "", $lastid);
+    $id = str_pad($idd + 1, 7, 0, STR_PAD_LEFT);
+    $number = 'ATK-'.$id;
 }
 
 ?>
+
+<?php
+ 
+  if($_SERVER["REQUEST_METHOD"]== "POST")
+  {
+      // $invoiceid = $_POST['invoiceid'];
+      // $prodname = $_POST['prodname'];
+      // $price = $_POST['price'];
+      $departmentType = $_POST['inputeparment'];
+      $sortdiscription = $_POST['inputSubject'];
+      $message = addslashes($_POST['inputMessage']);  
+      $fn = $_SESSION['ticket_fn']. $_SESSION['ticket_ln']; 
+      $employee_id = $_SESSION['id'];
+
+  
+      if(!$conn)
+      {
+          die("connection failed " . mysqli_connect_error());
+      }
+      else
+      {
+          $sql = "insert into ticket_incident(`id`, 
+          `ticket_number`, 
+          `u_id`, 
+          `ticket_caller`, 
+          `ticket_category`, 
+          `ticket_subcategory`, 
+          `ticket_service`, 
+          `ticket_config_item`, 
+          `ticket_short_discrip`, 
+          `ticket_discription`, 
+          `ticket_filedownload`, 
+          `ticket_contact_type`, 
+          `ticket_status`, 
+          `ticket_imapact`, 
+          `ticket_urgent`,
+          `ticket_priority`, 
+          `ticket_assign_group`,
+          `ticket_assign_to`, 
+          `ticket_department_id`, 
+          `ticket_timeofdate`,
+          `ticket_timeofdate_end`
+          )
+          VALUES('',
+          '',
+          '".$employee_id."',
+          '".$fn."',
+          '',
+          '',
+          '',
+          '',
+          '".$sortdiscription."',
+          '".$message."',
+          '',
+          '',
+          '3',
+          '',
+          '',
+          '',
+          '',
+          '',
+          '".$departmentType."',
+          NOW(),
+          '') ";
+          if(mysqli_query($conn,$sql))
+          {
+            $query = "SELECT ticket_number FROM ticket_incident ORDER BY ticket_number DESC";
+            $result = mysqli_query($conn,$query);
+            $row = mysqli_fetch_array($result);
+            $lastid = $row['id'];
+  
+              if(empty($lastid))
+              {
+                  $number = "E-0000001";
+              }
+              else
+              {
+                  $idd = str_replace("ATK-", "", $lastid);
+                  $id = str_pad($idd + 1, 7, 0, STR_PAD_LEFT);
+                  $number = 'ATK-'.$id;
+              }
+  
+          }
+          else
+          {
+              echo "Record Faileddd";
+          }
+      }
+  }
+    ?>
