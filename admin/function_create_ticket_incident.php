f273b8279b276e977dc78791475383d171cefe02
Alberto Flores <106653612+rjflores747@users.noreply.github.com>
2023-04-11 10:12:24 +0800

    updating

--- admin/function_create_ticket_incident.php
+++ admin/function_create_ticket_incident.php
@@ -4,21 +4,17 @@ $query = "SELECT id FROM ticket_incident ORDER BY id DESC";
 $result = mysqli_query($conn,$query);
 $row = mysqli_fetch_array($result);
 $lastid = $row['id'];
-
 if(empty($lastid))
 {
     $number = "ATK-0000001";
 }
 else
 {
-  
     $idd = str_replace("ATK-", "", $lastid);
     $id = str_pad($idd + 1, 7, 0, STR_PAD_LEFT);
     $number = 'ATK-'.$id;
 }
-
 ?>
-
 <?php
  
   if($_SERVER["REQUEST_METHOD"]== "POST")
@@ -31,10 +27,7 @@ else
       $message = addslashes($_POST['inputMessage']);  
       $fn = $_SESSION['ticket_fn']. $_SESSION['ticket_ln']; 
       $employee_id = $_SESSION['id'];
-
   
-
-
       if(!$conn)
       {
           die("connection failed " . mysqli_connect_error());
@@ -43,26 +36,25 @@ else
       {
           // var_dump( 
             $sql = "insert into ticket_incident(
-              `ticket_number`,
-              `u_id`,
-              `ticket_caller`,
-              `ticket_category`,
-              `ticket_subcategory`,
-              `ticket_service`,
-              `ticket_config_item`,
-              `ticket_short_discrip`,
-              `ticket_discription`,
-              `ticket_filedownload`,
-              `ticket_contact_type`,
-              `ticket_status`,
-              `ticket_imapact`,
-              `ticket_urgent`,
-              `ticket_priority`,
-              `ticket_assign_group`,
-              `ticket_assign_to`,
-              `ticket_department_id`,
-              `ticket_timeofdate`,
-              `ticket_timeofdate_end`
+          `ticket_number`, 
+          `u_id`, 
+          `ticket_caller`, 
+          `ticket_category`, 
+          `ticket_subcategory`, 
+          `ticket_service`, 
+          `ticket_config_item`, 
+          `ticket_short_discrip`, 
+          `ticket_discription`, 
+          `ticket_filedownload`, 
+          `ticket_contact_type`, 
+          `ticket_status`, 
+          `ticket_imapact`, 
+          `ticket_urgent`,
+          `ticket_priority`, 
+          `ticket_assign_group`,
+          `ticket_assign_to`, 
+          `ticket_department_id`, 
+          `ticket_timeofdate`
           )
           VALUES(
           '".$number."',
@@ -83,17 +75,17 @@ else
           '',
           '',
           '".$departmentType."',
-          NOW(),
-          ''
+          NOW()
           ) ";
           if(mysqli_query($conn,$sql))
           {
             $query = "SELECT id FROM ticket_incident ORDER BY id DESC";
             $result = mysqli_query($conn,$query);
             $row = mysqli_fetch_array($result);
-            
+        
+
             $lastid = $row['id'];
-  
+
               if(empty($lastid))
               {
                   $number = "ATK-0000001";
@@ -104,21 +96,16 @@ else
                   $id = str_pad($idd + 1, 7, 0, STR_PAD_LEFT);
                   $number = 'ATK-'.$id;
               }
-         
-              // header("location: ../admin/ticket_details_container.php?id=".$lastid);
-        
-
+  
           }
           else
           {
               echo "Record Faileddd";
           }
-          
-        // header("location: ./admin/ticket_details_container.php?id=".$lastid);
-        // require_once "../admin/ticket_details_container.php?id".$lastid;
-echo '<script>alert("added successfully.");
-		window.location="ticket_details_container.php?id="'.$lastid.'";
-</script>';
+
+        header("location: ../admin/ticket_details_container.php?id=".$lastid);
+
+
       }
   }
     ?>
