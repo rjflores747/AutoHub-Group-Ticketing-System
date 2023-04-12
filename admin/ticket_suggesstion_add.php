ae402e3059771b00d8b86fff006ef8261628efa1
Alberto Flores <106653612+rjflores747@users.noreply.github.com>
2023-04-12 11:30:08 +0800

    UPDATING FUNCATION INCEDENT AND SUGGESTION

--- admin/ticket_suggesstion_add.php
+++ admin/ticket_suggesstion_add.php
@@ -1,3 +1,26 @@
+<?php
+require_once '../connect.php';
+if (!isset($_SESSION["id"])) {
+  header("Location: index.php");
+  exit();
+ 
+}
+$message = '';
+
+if(count($_POST)>0) {
+  
+mysqli_query($conn,"INSERT INTO `ticket_suggestions`(`suggestions_name`, `suggestions_description`, `createdAt`, `suggestions_status`) VALUES ('". $_POST['suggestions_name'] . "','". $_POST['suggestions_description'] ."',NOW(),'1')");
+
+
+ // Insert visitor activity log into database 
+$ActivityLogs = mysqli_query($conn,"INSERT INTO `ticket_activity_logs`(`ticket_activity_uid`, `ticket_activity_name`, `ticket_activity_created_on`) VALUES ('".$_SESSION['u_id']."','Updating the Ticket Incident' ,NOW())");
+
+
+$message = "Record Modified Successfully";
+
+}
+
+?>
 
 <div class="container">
 	<div class="row">
@@ -41,12 +64,14 @@
 		<div class="col-md-6">
 			<h4>Add new subject</h4>
 			<hr>
-			<form method="post" action="../admin/ticket_suggesstion_db.php">
+			<!-- <form method="post" action="../admin/ticket_suggesstion_db.php"> -->
+			<form name="frmUser" method="post" action="">
+				
 				<div class="form-group">
-					<input type="text" name="suggestions_name" placeholder="suggestions Name" class="form-control" />
+					<input type="text" name="suggestions_name" placeholder="suggestions Name" class="form-control" required/>
 				</div>
 				<div class="form-group">
-					<input type="text" name="suggestions_description" placeholder="suggestions description" class="form-control" />
+					<input type="text" name="suggestions_description" placeholder="suggestions description" class="form-control" required/>
 				</div>
 				<div class="form-group">
 					<input name="suggestions_submit" class="btn btn-primary" type="submit" value="save record"/>
