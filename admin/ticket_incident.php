0fcc411935290fd6f7c5b22a815ac6c45d93dc59
Alberto Flores <106653612+rjflores747@users.noreply.github.com>
2023-05-16 14:11:47 +0800

    updating

--- admin/ticket_incident.php
+++ admin/ticket_incident.php
@@ -2,27 +2,24 @@
 require_once '../connect.php'; 
 
 if (!isset($_SESSION["id"])) {
-  header("Location: index.php");
+  header("Location: index.php"); 
   exit();
- 
-}
-
+} 
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>AutoHubGroup | TicketingSystem</title>
-  <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
   <?php include '../link-required-start.php';?>
           <style>  
        
            .ul-main{  
                 
-                background-color:#eee;   
+                background-color:#eee;  
                 cursor:pointer;  
                 
                 overflow: scroll;
@@ -30,22 +27,14 @@ if (!isset($_SESSION["id"])) {
            .li-main{  
                 padding:12px;  
            }  
-         
-     
-
-           .inputSubject {
-       font-family: 'Times New Roman';
-       font-size: 12px;
-     }
-
            </style> 
 <body class="hold-transition light sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
 <div class="wrapper">
 
   <!-- Preloader -->
-  <!-- <div class="preloader flex-column justify-content-center align-items-center">
+  <div class="preloader flex-column justify-content-center align-items-center">
     <img class="animation__wobble" src="../img/autohub-logo.png" alt="AutohubLogo" height="60" width="60">
-  </div> -->
+  </div>
 <!-- navbar -->
 <?php include '../navbar/navbar.php';?>
 
@@ -56,6 +45,9 @@ if (!isset($_SESSION["id"])) {
        <!-- Content Header (Page header) -->
   <!-- <php include '../navbar/content-header.php';> -->
             <!-- Main Content-->
+          
+ 
+  <!-- Main content -->
   <section class="content">
       <div class="container-fluid">
         
@@ -78,63 +70,48 @@ if (!isset($_SESSION["id"])) {
             </div>
           </div>
           <div class="col-7"> 
-             <!-- <form method="post"> -->
-                <form action="../admin/function_create_ticket_incident.php" method="POST">
-                  <div class="form-group">
-                    <label for="inputeparment">Department</label>
-                    <select class="form-control select2bs4"  name="inputeparment" id="inputeparment" style="width: 100%;" required>
-                                <option value="" >----- Select Department -----</option>
-                                <?php
-                                $array_data ['uri'] = 'https://autohub.ph/connect/api/v1/asa/api.php';
-                                $array_data['parameters'] = http_build_query(array('key'=>'99799116300681219'));
-                                
-                                  $result = Utility::curl($array_data);
-                                  $department_array = json_decode($result,true);
-                                  foreach($department_array as $row1)
-                                  {
-                                    ?>
+             <form method="post">
+                 <!--<form action="../admin/function_create_ticket_incident.php" method="POST"> -->
+                <div class="form-group">
+                  <label for="inputeparment">Department</label>
+                   <select class="form-control select2bs4"  name="inputeparment" id="inputeparment" style="width: 100%;" require>
+                              <option value="" require>----- Select Department -----</option>
+                              <?php
+                              $array_data ['uri'] = 'https://autohub.ph/connect/api/v1/asa/api.php';
+                              $array_data['parameters'] = http_build_query(array('key'=>'99799116300681219'));
+                               
+                                $result = Utility::curl($array_data);
+                                $department_array = json_decode($result,true);
+                                foreach($department_array as $row1)
+                                {
+                                  ?>
 
-                                    <option value="<?php echo $row1['id']?>">
-                                    <?php echo $row1['dept_name'];?>
-                                  </option>
-                                    <?php 
+                                  <option value="<?php echo $row1['id']?>">
+                                  <?php echo $row1['dept_name'];?>
+                                </option>
+                                  <?php 
 
-                                  }
-                                    ?>
-                                </select>
-                    <!-- <input type="text" id="inputeparment" name="inputeparment" class="form-control" /> -->
-                  </div>
-                  
+                                }
+                                   ?>
+                              </select>
+                  <!-- <input type="text" id="inputeparment" name="inputeparment" class="form-control" /> -->
+                </div>
+                
+                <div class="form-group">
+                  <label for="inputSubject">Subject</label>
+                  <!-- <input type="text" id="inputSubject" name="inputSubject" class="form-control"require /> -->
+                  <input type="text" name="inputSubject" id="inputSubject" class="form-control" placeholder="Enter Subject" />  
+                  <div id="subjectList"></div>  
+           
+                </div>
+             
                   <div class="form-group">
-                    <label for="inputSubject">Subject</label>
-                    <!-- <input type="text" id="inputSubject" name="inputSubject" class="form-control"require /> -->
-                    <!-- <input type="text" name="inputSubject" id="inputSubject" class="form-control" placeholder="Enter Subject"required />  
-                    <div id="subjectList"></div>   -->
-                                
-                                        <!-- Autocomplete input field -->
-                    <input id="searchInput"  name="inputSubject"  placeholder="Enter Subject..." class="form-control" autocomplete="off" required>
-
-                    <!-- Hidden input to store selected user's ID -->  
-                    <input type="hidden" id="userID" name="userID" class="form-control" value=""/>
-                    
-                  </div>
-                  <div class="form-group">
-                    <label for="inputSubject">Service-Level Agreement(SLA)</label>
-                    
-                                
-                                        <!-- Autocomplete input field -->
-                    <input id="inputSla"  name="inputSla"  placeholder="" class="form-control select2bs4" autocomplete="off" readonly required>
-
-                    
-                  </div>  
-              
-                    <div class="form-group">
-                    <label for="inputMessage">Description</label>
-                    <textarea id="inputMessage" class="form-control" name="inputMessage" rows="4"required></textarea>
-                  </div>
-                  <div class="form-group">
-                    <input type="submit" id="button-send-message-detail" class="btn btn-primary" name="submit" value="Send message">
-                  </div>
+                  <label for="inputMessage">Discription</label>
+                  <textarea id="inputMessage" class="form-control" name="inputMessage" rows="4"require></textarea>
+                </div>
+                <div class="form-group">
+                  <input type="submit" id="button-send-message-details"  class="btn btn-primary" name="submit" value="Send message">
+                </div>
                 </form>
               
             <!-- </form> -->
@@ -163,102 +140,8 @@ if (!isset($_SESSION["id"])) {
 <!-- ./wrapper -->
 <!-- link required scripts -->
 <?php include '../link-required-scripts-end.php';?>
-<!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<!-- jQuery UI library -->
-<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.13.2/themes/smoothness/jquery-ui.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.13.2/jquery-ui.min.js"></script>
-<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-
 </body>
 </html>
-
-<script>
-$(document).ready(function(){
-    $("#searchInput").autocomplete({
-        source: "fetchUsers.php",
-        minLength: 1,
-        select: function(event, ui) {
-            $("#searchInput").val(ui.item.value);
-            $("#inputSla").val(ui.item.value2);
-            $("#inputMessage").val(ui.item.value1);
-            $("#userID").val(ui.item.id);
-        }
-    }).data("ui-autocomplete")._renderItem = function( ul, item ) {
-    return $( "<li class='ui-autocomplete-row'></li>" )
-        .data( "item.autocomplete", item )
-        .append( item.label )
-        .appendTo( ul );
-    };
-});
-</script>
-
-<!-- <script>
-    //variable
-    var variable_department = $('#inputeparment').val();
-            var variable_subject = $('#inputSubject').val();
-            var variable_message = $('#inputMessage').val();
-            var variable_sla = $('#inputSla').val();
-            
-
-// $('#button-send-message-details').on('click',function(){
-  $('#button-send-message-detail').on('click', function(){ 
-   
-    Swal.fire({
-      title: "Your ticket has been submitted",
-          text: "Please wait for your ticket to be assigned",
-          icon: "warning",
-          showCancelButton: true,
-          confirmButtonColor: "#3085d6",
-          cancelButtonColor: "#d33",
-          confirmButtonText: "Yes, remove it!",
-          showClass: {
-            backdrop: "swal2-noanimation", // disable backdrop animation
-            popup: "", // disable popup animation
-            icon: "", // disable icon animation
-          },
-          hideClass: {
-            popup: "", // disable popup fade-out animation
-          },
-        }).then((result) => {
-          if (result.isConfirmed) {
-          
-            $.ajax({
-              url: "function_create_ticket_incident.php",
-              data: {
-                var_department: variable_department, 
-                  var_subject: variable_subject, 
-                  var_message: variable_message, 
-                  var_sla: variable_sla, 
-                  type: 1 // login status
-
-              },
-              type: "POST",
-              dataType: "json",
-              beforeSend: function () {
-                // toast("info", "Updating...");
-              },
-              success: function (result) {
-                Toast.fire({
-                  icon: 'success',
-                  title: 'SucesssFul .'
-                }); 
-                
-                $("#example1").DataTable().ajax.reload();
-              },
-              error: function () {
-    //             alert('');
-    // return;
-                // toast("error", "Error has occurred. Try again.");
-              },
-            });
-          }
-    })
-  
-  })
-   
-  
-  </script> -->
 
 <script>
           var Toast = null;
@@ -271,15 +154,19 @@ $(document).ready(function(){
               timer: 3000
             });
                //Initialize Select2 Elements
-           
+    $('.select2').select2()
+
+            //Initialize Select2 Elements
+            $('.select2bs4').select2({
+              theme: 'bootstrap4'
+            })
             $('#button-send-message-details').on('click', function(){ 
          
             //variable
             var variable_department = $('#inputeparment').val();
             var variable_subject = $('#inputSubject').val();
             var variable_message = $('#inputMessage').val();
-            var variable_sla = $('#inputMessage').val();
-            
+
 
           // REQUIREMENTS
           if(variable_department == ""){
@@ -300,23 +187,16 @@ $(document).ready(function(){
 
             return;
           }
-          if(variable_sla == ""){
-            toastr.remove();
-            toastr.error("Sla cannot be empty", "Incomplete data");
-
-            return;
-          }
           // Login Validation
           $.ajax({
-              // url:"function_create_ticket_incident.php",  
+              url:"function_create_ticket_incident.php",  
               type:"POST", 
               dataType:"json",
               data: {
                   var_department: variable_department, 
                   var_subject: variable_subject, 
                   var_message: variable_message, 
-                  var_sla: variable_sla, 
-                  type: 1 // login status
+                  type: 1 // insert status
               },
               beforeSend: function(){
                   // $('#loading-view').attr('hidden', false);
@@ -326,17 +206,8 @@ $(document).ready(function(){
               success: function(result){
             // alert(result.status);
                   if(result.status == 1){ // success
-                     
-                      // toastr.success();
-                      // toastr.error("Add successfully", "Complete data");
-                    
-                        swal({
-                            title: "Your ticket has been submitted",
-                            text: "Please wait for your ticket to be assigned",
-                            icon: "success",
-                            button: "OK"
-                        });
-          
+                      toastr.success();
+                      toastr.error("Add successfully","Complete data");
                       
                       window.location.href='ticket_details_container.php?id='+result.id;
                       // $('#modal-finance-add-fni').modal('hide');
@@ -363,20 +234,21 @@ $(document).ready(function(){
               //         toastr.error("Wrong Credentials");
               // return;
               //     } 
+              
               }
           })
 
         });
       })
 </script>
-<!-- <script>  
+<script>  
  $(document).ready(function(){  
       $('#inputSubject').keyup(function(){  
            var query = $(this).val();  
            if(query != '')  
            {  
                 $.ajax({  
-                     url:"../admin/search.php",  
+                     url:"../admin/testing/search.php",  
                      method:"POST",  
                      data:{query:query},  
                      success:function(data)  
@@ -392,4 +264,4 @@ $(document).ready(function(){
            $('#subjectList').fadeOut();  
       });  
  });  
- </script>   -->
+ </script>  
